46c4bdeadb7f0ff60d58ac02fbccab6368020856
Chiara Coetzee <chiaracoetzee@gmail.com>
2026-09-18 18:37:37 -0700

    feat(mobile): update app title to Fluxer Temple and default instance to temple.hypersystem.xyz

--- lib/core/instance/instance_config_snapshot.dart
+++ lib/core/instance/instance_config_snapshot.dart
@@ -38,8 +38,8 @@ class InstanceConfigSnapshot {
   factory InstanceConfigSnapshot.officialDefault() {
     return const InstanceConfigSnapshot(
       apiBaseUrl: InstanceConstants.defaultApiBaseUrl,
-      gatewayUrl: '',
-      displayDomain: 'fluxer.app',
+      gatewayUrl: 'wss://temple.hypersystem.xyz/gateway',
+      displayDomain: 'temple.hypersystem.xyz',
     );
   }
 
