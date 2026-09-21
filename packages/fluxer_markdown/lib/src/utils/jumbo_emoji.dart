4bb5bd6d0a28313ea3fe94d25ba8d0c27b2ae3ad
Chiara Coetzee <chiaracoetzee@gmail.com>
2026-09-20 17:40:47 -0700

    style(chat): scale standalone emojis by 25% and reduce mention font weight to w400

--- packages/fluxer_markdown/lib/src/utils/jumbo_emoji.dart
+++ packages/fluxer_markdown/lib/src/utils/jumbo_emoji.dart
@@ -1,7 +1,7 @@
 const kFluxerMarkdownJumboMaxCount = 30;
 const kFluxerMarkdownEmojiSizeMultiplier = 1.5;
 const kFluxerMarkdownEmojiSizeNormal = 24.0;
-const kFluxerMarkdownEmojiSizeJumbo = 48.0;
+const kFluxerMarkdownEmojiSizeJumbo = 60.0;
 const kFluxerMarkdownRestrictedInlineEmojiEm = 1.25;
 
 double fluxerMarkdownEmojiSize({
