11784ae9c11e9995941a550c3bb637ded85d48be
Chiara Coetzee <chiaracoetzee@gmail.com>
2026-09-22 11:16:05 -0700

    fix(ui): disable top-right canary corner banner

--- lib/shared/widgets/beta_banner.dart
+++ lib/shared/widgets/beta_banner.dart
@@ -1,12 +1,4 @@
-import 'package:fluxer_app/core/build/app_build_config.dart';
 import 'package:fluxer_app/material_ui.dart';
-
-const Color _kCanaryIconColor = Color.from(
-  alpha: 1,
-  red: 0.95215,
-  green: 0.66553,
-  blue: 0.23462,
-);
 
 class BetaBanner extends StatelessWidget {
   const BetaBanner({required this.child, super.key});
@@ -15,19 +7,6 @@ class BetaBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (!AppBuildConfig.isCanary) {
-      return child;
-    }
-    return Banner(
-      location: BannerLocation.topEnd,
-      message: 'CANARY',
-      color: _kCanaryIconColor,
-      textStyle: Theme.of(context).textTheme.displayLarge!.copyWith(
-        fontSize: 10,
-        color: Colors.black,
-        fontWeight: FontWeight.w600,
-      ),
-      child: child,
-    );
+    return child;
   }
 }
